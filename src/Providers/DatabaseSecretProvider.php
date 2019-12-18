046e0ff9d0c01859af9abad46d189cef4530ece1
Diego <=>
2019-12-18 12:54:04 -0300

    first commit

---
+++ src/Providers/DatabaseSecretProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace litermi\secretDBdriver\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseSecretProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/secret.php' => config_path('secret.php'),
+        ]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
